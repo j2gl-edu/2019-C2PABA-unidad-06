8c8f5f3685200a26a8df9f393839f13d21d44a28
Juan J. Garcia <j2gl.edu@gmail.com>
2019-05-01 19:07:21 -0600

    Clean up

--- src/main/java/edu/advanced/unit6/sort/QuickSort.java
+++ src/main/java/edu/advanced/unit6/sort/QuickSort.java
@@ -2,15 +2,15 @@ package edu.advanced.unit6.sort;
 
 public class QuickSort {
 
+    private static boolean DEBUG_ENABLED = true;
     private int array[];
-    private int length;
 
-    public void sort(int[] inputArr) {
+    private void sort(int[] inputArr) {
         if (inputArr == null || inputArr.length == 0) {
             return;
         }
         this.array = inputArr;
-        length = inputArr.length;
+        int length = inputArr.length;
         quickSort(0, length - 1);
     }
 
@@ -18,6 +18,9 @@ public class QuickSort {
         int i = lowerIndex;
         int j = higherIndex;
         int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
+        log("lowerIndex   = " + lowerIndex);
+        log("higherIndex  = " + higherIndex);
+        log("pivot        = " + pivot + "");
         while (i <= j) {
             while (array[i] < pivot) {
                 i++;
@@ -26,13 +29,17 @@ public class QuickSort {
                 j--;
             }
             if (i <= j) {
+                log("Array Before = " + convertToString(array));
                 exchangeNumbers(i, j);
-                // move index to next position on both sides
+                log("Array After  = " + convertToString(array));
                 i++;
                 j--;
+                // log("Moving i = " + i + ", j = " + j);
             }
         }
-        // call quickSort() method recursively
+
+        log("i = " + i + ", j = " + j);
+
         if (lowerIndex < j) {
             quickSort(lowerIndex, j);
         }
@@ -47,13 +54,28 @@ public class QuickSort {
         array[j] = temp;
     }
 
+    private void log(String s) {
+        if (DEBUG_ENABLED) {
+            System.out.println(s);
+        }
+    }
+
+    private static String convertToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int i : array) {
+            sb.append(i).append(" ");
+        }
+        return sb.toString();
+    }
+
     public static void main(String a[]) {
         QuickSort sorter = new QuickSort();
-        int[] input = { 24, 2, 45, 20, 56, 75, 2, 56, 99, 53, 12 };
+        //int[] input = {3, 7, 5, 8, 2};
+        int[] input = {3, 7, 8, 5, 2, 1, 9, 5, 4};
+        //int[] input = { 24, 2, 45, 20, 56, 75, 2, 56, 99, 53, 12 };
+
+        System.out.println(convertToString(input));
         sorter.sort(input);
-        for (int i : input) {
-            System.out.print(i);
-            System.out.print(" ");
-        }
+        System.out.println(convertToString(input));
     }
 }
